f098a667d7a56cbee405132e6d2a0175de443423
Dima Lupyak <dima.lupyak@gmail.com>
2016-07-05 15:05:00 +0300

    Realized product CRUD poerations

--- CarStoreBusinessLogic/ProductRepository.cs
+++ CarStoreBusinessLogic/ProductRepository.cs
@@ -4,39 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CarStoreDataBaseEntities;
+using System.Data;
 
 namespace CarStoreBusinessLogic
 {
     class ProductRepository : IProductRepository
     {
+        #region Singleton
+        protected static ProductRepository instance = null;
+        public static ProductRepository Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ProductRepository();
+                return instance;
+
+            }
+        }
+        #endregion
+        #region Constructors
+        protected ProductRepository() { }
+        #endregion
+        #region IChallengeRepository CRUD Methods
         public void Create(Product item)
         {
-            throw new NotImplementedException();
+            using (CarStoreDBEntities entities = new CarStoreDBEntities())
+            {
+                entities.Products.Add(item);
+                entities.SaveChanges();
+            }
         }
-
+        public List<Product> Read()
+        {
+            using (CarStoreDBEntities entities = new CarStoreDBEntities())
+            {
+                return entities.Products.ToList();
+            }
+        }
+        public void Update(Product item)
+        {
+            using (CarStoreDBEntities entities = new CarStoreDBEntities())
+            {
+                entities.Products.Attach(item);
+                entities.Entry(item).State = EntityState.Modified;
+                entities.SaveChanges();
+            }
+        }
+        
         public void Destroy(Product item)
         {
-            throw new NotImplementedException();
+            using (CarStoreDBEntities entities = new CarStoreDBEntities())
+            {
+                entities.Products.Attach(item);
+                entities.Products.Remove(item);
+                entities.SaveChanges();
+            }
         }
+        #endregion
 
         public List<ProductCategory> GetCategories()
         {
-            throw new NotImplementedException();
+            using (CarStoreDBEntities entities = new CarStoreDBEntities())
+            {
+                return entities.ProductCategories.ToList();
+            }
         }
-
         public List<ProductMaterial> GetMaterials()
         {
-            throw new NotImplementedException();
-        }
-
-        public List<Product> Read()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Product item)
-        {
-            throw new NotImplementedException();
-        }
+            using (CarStoreDBEntities entities = new CarStoreDBEntities())
+            {
+                return entities.ProductMaterials.ToList();
+            }
+        } 
     }
 }
