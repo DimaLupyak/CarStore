d4698c1ce59b4c0a8aec13f65288f89861c5abf5
Dima Lupyak <dima.lupyak@gmail.com>
2016-07-07 18:36:07 +0300

    Added cart

---
+++ CarStoreViewModels/Cart.cs
@@ -0,0 +1,60 @@
+﻿using CarStoreViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarStoreViewModels
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(ProductViewModel product, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(g => g.Product.ID == product.ID)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(ProductViewModel product)
+        {
+            lineCollection.RemoveAll(l => l.Product.ID == product.ID);
+        }
+
+        public double ComputeTotalValue()
+        {
+            return lineCollection.Sum(p => p.Product.Price * p.Quantity);
+
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+
+    public class CartLine
+    {
+        public ProductViewModel Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ CarStoreWeb/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using CarStoreService;
+using CarStoreViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarStoreWeb.Controllers
+{
+    public class CartController : Controller
+    {
+        IProductService productService;
+        public int pageSize = 4;
+
+        public CartController()
+        {
+            productService = new ProductService();
+        }
+
+        public CartController(IProductService service)
+        {
+            productService = service;
+        }
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
+        {
+            ProductViewModel product = productService.Read()
+                .FirstOrDefault(p => p.ID == productId);
+
+            if (product != null)
+            {
+                cart.AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
+        {
+            ProductViewModel product = productService.Read()
+                .FirstOrDefault(p => p.ID == productId);
+
+            if (product != null)
+            {
+                cart.RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+    }
+}
